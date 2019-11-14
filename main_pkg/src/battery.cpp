ef296326d834b709752087a2f851bf31d0469edf
vmatth <vmatth19@student.aau.dk>
2019-11-14 12:59:03 +0100

    Battery2

--- main_pkg/src/battery.cpp
+++ main_pkg/src/battery.cpp
@@ -5,18 +5,26 @@
 
 int kobuki_max_charge = 168; //check senere ved at checke volt når fuld, og indsæt selv eget tal.
 
-void BatteryStateCallback(const kobuki_msgs::SensorState::ConstPtr &msg)
+void BatteryStateCallback(const kobuki_msgs::SensorState::ConstPtr &msg) //Callback når battery ændres
 {
-    float batterypct = float(msg->battery) / float(kobuki_max_charge) * 100;
-    ROS_INFO("Volt: %d", msg->battery);
-    ROS_INFO("Battery: %i pct", int(round(batterypct)));
+    float batterypct = float(msg->battery) / float(kobuki_max_charge) * 100; //Udregn % battery
+    ROS_INFO("Volt: %d", msg->battery); //Output batteriet (i Volt)
+    ROS_INFO("Battery: %i pct", int(round(batterypct))); //Output battery % (Rundet op og uden decimaler)
     
-    if (int (msg->charger) == 1){
+    ROS_INFO("dock status: %i", msg->charger);
+
+    if (int (msg->charger) == 6){ //Hvis den er i docking
         ROS_INFO("Den er i opladeren");
     }
-    else {
+    else if(int ( msg->charger) == 0){ //Hvis output er 0
         ROS_INFO("Den er ikke i opladeren");
+        //system("roslaunch kobuki_auto_docking minimal.launch --screen");
+        //system("roslaunch kobuki_auto_docking activate.launch --screen");
     }
+    else if(int (msg->charger) == 2){
+        ROS_INFO("Ladet op!");
+    }
+
 /*
     if (int (msg->charger) == 1 && batterypct > 97){
         // Påbegynd næste opgave hvis den er i docken og har en ladningsprocent højere end 97
@@ -26,14 +34,16 @@ void BatteryStateCallback(const kobuki_msgs::SensorState::ConstPtr &msg)
 */
 }
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) //Main
 {
     std::cout << "Checking Battery State" << std::endl;
 
+    //Initiate ros
     ros::init(argc, argv, "kobuki_battery");
 
     ros::NodeHandle n;
 
+    //Subscribe til battery
     ros::Subscriber sub = n.subscribe("/mobile_base/sensors/core", 1000, BatteryStateCallback);
 
     ros::spin();
