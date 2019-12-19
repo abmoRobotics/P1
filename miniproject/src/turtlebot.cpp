8efc264d74dedcca4bd0f9a9a39b83f7716f7573
Marie B <mbages19@student.aau.dk>
2019-12-19 15:20:40 +0100

    jallah hadddda

--- miniproject/src/turtlebot.cpp
+++ miniproject/src/turtlebot.cpp
@@ -1,8 +1,12 @@
+
 #include <ros/ros.h>
 #include <iostream>
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/ButtonEvent.h>
 #include <kobuki_msgs/SensorState.h>
+#include <std_srvs/Trigger.h>
+#include <sound_play/sound_play.h>
+
 
 class Turtlebot
 {
@@ -38,16 +42,33 @@ class Turtlebot
         }
     }
 
-    Turtlebot()
+  function square (std_srvs::Trigger::Request &req,
+               std_srvs::Trigger::Response &res)
     {
+        //axel go crazy
+    }
+  function LED (std_srvs::Trigger::Request &req,
+            std_srvs::Trigger::Response &res)
+    {
+
     }
 
+  function sing (std_srvs::Trigger::Request &req,
+             std_srvs::Trigger::Response &res)
+    {
+      
+
+    }
 }
 ;
 int main(int argc, char *argv[]){
     std::cout << "Tryk på B0 for at køre i en firkant" << std::endl;
-
+    ros::NodeHandle n;
     ros::init(argc, argv, "Square");
+  //hør lige om functionen den kalder
+    ros::ServiceServer server_square = n.advertiseService("square", square);
+    ros::ServiceServer server_LED = n.advertiseService("LED", LED);
+    ros::ServiceServer server_sing = n.advertiseService("sing", sing);
 
     return 0;
 }
