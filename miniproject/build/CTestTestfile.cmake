f9ea08efce6c2f3105e6c45991f3229c9cc2a69c
Marie B <mbages19@student.aau.dk>
2019-12-19 14:10:06 +0100

    jallllllaaaaah

---
+++ miniproject/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/p1_ws/src/miniproject/src
+# Build directory: /home/ros/p1_ws/src/miniproject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ miniproject/src/interface.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <std_srvs/Trigger.h>
+
+//Service
+ros::NodeHandle n;
+
+ros::ServiceClient client_square = n.serviceClient<std_srvs::Trigger>("square");
+ros::ServiceClient client_LED = n.serviceClient<std_srvs::Trigger>("LED");
+ros::ServiceClient client_sing = n.serviceClient<std_srvs::Trigger>("sing");
+
+
+std_srvs::Trigger square;
+std_srvs::Trigger LED;
+std_srvs::Trigger sing;
+
+void makeasquare()
+{
+   if (client_square.call(square))
+   {
+       ROS_INFO("Task received");
+   }
+   else
+   {
+       ROS_ERROR("Service was not called");
+   }
+          
+};
+
+
+void startLED()
+{
+    if (client_square.call(LED))
+    {
+        ROS_INFO("Task received");
+    }
+    else
+    {
+        ROS_ERROR("Service was not called");
+    }
+ 
+};
+
+void robotsing()
+{
+    if (client_square.call(sing))
+    {
+       ROS_INFO("Task received"); 
+    }
+    else
+    {
+        ROS_ERROR("Service was not called");
+    }
+       
+ };
+
+
+
+void menu()
+{
+    int c=1;
+
+    while(c !=0)
+    {
+        system("clear");
+        std::cout << "1. Make robot move in a square" << std::endl;
+        std::cout << "2. Make LED light up" << std::endl;
+        std::cout << "3. Make robot sing" << std::endl;
+        std::cout << "-----------------------------" << std::endl;
+        std::cout << "Select an option" << std::endl;
+
+        std::cin >> c;
+
+        while (1>c>3)
+        {
+            std::cin >> c;
+        }
+        
+        switch (c)
+        {
+        case 1:
+            makeasquare();
+            break;
+        case 2:
+            startLED();
+            break;
+        case 3:
+            robotsing();
+            break;
+        default:
+            std::cout << "Choose option 1, 2 or 3" << std::endl;
+            break;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+   ros::init(argc, argv, "interface");
+   menu();
+   ros::spin();
+}
